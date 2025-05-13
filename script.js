275f0b03f98f0b74f2e79d1a681c6b9ad0d2a293
Rutu2012 <sanghanirutu944@gmail.com>
2025-05-13 13:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function validEmail(str) {
-  //your JS code here.
+function validEmail(str:string):boolean {
+  if(!str || str.trim()===""){
+	  const emailRegex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z]{2,}$/;
+	  return emaillRegex.test(str);
+  }
 }
-
+validEmail('abc@example.com');
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
